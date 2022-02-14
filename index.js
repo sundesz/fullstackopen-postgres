52964f4414dc41c66fc2afcecab81f6d4b90ee9e
Sandesh Hyoju <sundesz@gmail.com>
2022-02-14 23:25:15 +0200

    Add solutions of Part13 Exercise 13.4 postgres

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 require('dotenv').config()
-const { Sequelize } = require('sequelize')
+const { Sequelize, Model, DataTypes } = require('sequelize')
+const express = require('express')
+
+const app = express()
+app.use(express.json())
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialectOptions: {
@@ -10,14 +14,119 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
   },
 })
 
-const main = async () => {
-  try {
-    await sequelize.authenticate()
-    console.log('Connection has been established successfully.')
-    sequelize.close()
-  } catch (error) {
-    console.log('Unable to connect to database:', error)
-  }
-}
+// const main = async () => {
+//   try {
+//     await sequelize.authenticate()
+//     console.log('Connection has been established successfully.')
+//     sequelize.close()
+//   } catch (error) {
+//     console.log('Unable to connect to database:', error)
+//   }
+// }
 
-main()
+// main()
+
+class Blog extends Model {}
+Blog.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    author: {
+      type: DataTypes.STRING,
+    },
+    url: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    likes: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+    },
+  },
+  {
+    sequelize,
+    underscored: true,
+    timestamps: false,
+    modelName: 'blog',
+  }
+)
+
+Blog.sync()
+
+app.get('/', (req, res) => {
+  res.send('<h1>Sandesh Blog</h1>')
+})
+
+app.get('/api/blogs', async (req, res) => {
+  try {
+    const blogs = await Blog.findAll()
+    console.log(blogs.map((b) => b.toJSON()))
+    console.log(JSON.stringify(blogs, null, 2))
+    res.json(blogs)
+  } catch (error) {
+    console.error('error: ', error)
+  }
+})
+
+app.post('/api/blogs', async (req, res) => {
+  try {
+    const blog = await Blog.create(req.body)
+    res.json(blog)
+  } catch (error) {
+    console.error('error: ', error)
+  }
+})
+
+app.get('/api/blogs/:id', async (req, res) => {
+  try {
+    const blog = await Blog.findByPk(req.params.id)
+    if (blog) {
+      return res.json(blog)
+    }
+
+    res.status(404).end()
+  } catch (error) {
+    console.error('error: ', error)
+  }
+})
+
+app.put('/api/blogs/:id', async (req, res) => {
+  try {
+    const blog = await Blog.findByPk(req.params.id)
+    if (blog) {
+      blog.likes = req.body.likes
+      await blog.save()
+      return res.json(blog)
+    }
+
+    res.status(404).end()
+  } catch (error) {
+    console.error('error: ', error)
+  }
+})
+
+app.delete('/api/blogs/:id', async (req, res) => {
+  try {
+    const blog = await Blog.findByPk(req.params.id)
+    if (blog) {
+      await blog.destroy()
+      return res.status(204).end()
+    }
+
+    res.status(404).end()
+  } catch (error) {
+    console.error('error: ', error)
+  }
+})
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log('listening on port ', PORT)
+})
