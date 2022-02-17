dccb5006dcab8012d3b070999cc1cc2e4c9a6867
Sandesh Hyoju <sundesz@gmail.com>
2022-02-18 01:56:00 +0200

    Change http request method from POST to DELETE

--- controllers/logout.js
+++ controllers/logout.js
@@ -3,18 +3,21 @@ const { Session } = require('../models')
 
 const logoutRouter = require('express').Router()
 
-logoutRouter.post('/', tokenExtractor, async (req, res) => {
-  const session = await Session.findOne({
-    attributes: ['id', 'isValid', 'userId'],
-    where: { userId: req.decodedToken.id },
-  })
+logoutRouter.delete('/', tokenExtractor, async (req, res) => {
+  // const session = await Session.findOne({
+  //   attributes: ['id', 'isValid', 'userId'],
+  //   where: { userId: req.decodedToken.id },
+  // })
 
-  if (session) {
-    session.isValid = false
-    await session.save()
+  // if (session) {
+  //   session.isValid = false
+  //   await session.save()
 
-    res.json({ message: 'Logout successful' })
-  }
+  //   res.json({ message: 'Logout successful' })
+  // }
+
+  await Session.destroy({ where: { userId: req.decodedToken.id } })
+  res.json({ message: 'Logout successful' })
 })
 
 module.exports = logoutRouter
